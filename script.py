c286776d0faf5e7bba972c0f9bfb20f3ae3730be
Aaron England <aaron.england.dev@gmail.com>
2020-11-30 10:35:25 -0700

    edit gitignore

---
+++ script.py
@@ -0,0 +1,14 @@
+# scraping tweets
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import tweepy
+import json
+import pandas as pd
+import csv
+import re
+from textblob import TextBlob
+import string
+import preprocessor as p
+import os
+import time
+
